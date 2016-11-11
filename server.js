f0b53025f6eaf36bb25a8a31ffd7f4eccf955a9e
maimous@gmail.com <stefaleon@gmail.com>
2016-11-11 15:14:24 +0200

    added views and style

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 
+// when the next line is not commented out, main route is served with index.html via express.static
+app.use(express.static('public'));
+
+// main route
 app.get('/', function(req, res) {
-	res.send('This is the main route.');
+	 res.send('This is the main route.');
+	// when use(express.static) is active the line above is overrided
+});
+
+// test route to EJSindex.ejs 
+app.get('/ejs-test', function(req, res) {
+	res.render('EJSindex');
 });
 
 app.listen(PORT, process.env.IP, function(){
