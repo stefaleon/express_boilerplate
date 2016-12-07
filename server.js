e1cc4cdb9aaff4ae109e6ca63c0c613ef17f1495
maimous@gmail.com <stefaleon@gmail.com>
2016-12-07 16:37:21 +0200

    add partials and navbar

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-// when the next line is not commented out, main route is served with index.html via express.static
+
 app.use(express.static(__dirname + '/public'));
 
 // main route
 app.get('/', (req, res) => {
-	 res.send('This is the main route.');
-	// when use(express.static) is active the line above is overrided
+	 res.render('main');
 });
 
-// test route to EJSindex.ejs 
-app.get('/ejs-test', (req, res) => {
-	res.render('EJSindex');
+// list polls
+app.get('/about', (req, res) => {
+	res.render('about');
 });
 
 app.listen(PORT, process.env.IP, () => {
