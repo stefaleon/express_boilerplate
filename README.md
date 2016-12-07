da64ca37e0a0063af251900509afa376f1158766
maimous@gmail.com <stefaleon@gmail.com>
2016-12-07 12:49:15 +0200

    update, use ES6 syntax

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Basic Express boilerplate with EJS support.
-### dependencies:
-* "ejs": "^2.5.2"
-* "express": "^4.14.0"
+
+* Express
+* EJS
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
 // when the next line is not commented out, main route is served with index.html via express.static
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // main route
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 	 res.send('This is the main route.');
 	// when use(express.static) is active the line above is overrided
 });
 
 // test route to EJSindex.ejs 
-app.get('/ejs-test', function(req, res) {
+app.get('/ejs-test', (req, res) => {
 	res.render('EJSindex');
 });
 
-app.listen(PORT, process.env.IP, function(){
+app.listen(PORT, process.env.IP, () => {
     console.log('Server started on port', PORT);
 });
